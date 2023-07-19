c13652f0fca34caa8c2877f2b8317d0c2dcf1ddc
Chukwuemeka Nkwo <emekankwo49@gmail.com>
2023-07-19 02:29:09 +0100

    implemented RTK query for easy API consumption and mutations

---
+++ src/redux/queryInterceptors.jsx
@@ -0,0 +1,50 @@
+import { fetchBaseQuery } from "@reduxjs/toolkit/dist/query";
+
+const baseUrl = process.env.REACT_APP_SERVER_URL;
+export const baseQuery = fetchBaseQuery({
+  baseUrl: `${baseUrl}`,
+  prepareHeaders: (headers) => {
+    const token = sessionStorage.getItem("authToken");
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
+
+export const baseQueryWithInterceptor = async (args, api, extraOptions) => {
+  const result = await baseQuery(args, api, extraOptions);
+  // Check if the response has a status code of 401
+  if (result.error?.status === 401 || result.error?.originalStatus === 401) {
+    sessionStorage.removeItem("authToken");
+    window.location.href = "/notAuthorized";
+  }
+  if (result.error?.status === 404 || result.error?.originalStatus === 404) {
+    window.location.href = "*";
+  }
+  if (result.error?.status === 503 || result.error?.originalStatus === 503) {
+    sessionStorage.removeItem("authToken");
+    window.location.href = "/underMaintenance";
+  }
+
+  if (result.error?.status === 500 || result.error?.originalStatus === 500) {
+    sessionStorage.removeItem("authToken");
+    window.location.href = "/serverError";
+  }
+  return result;
+};
+
+export const baseQueryForAuth = async (args, api, extraOptions) => {
+  const result = await baseQuery(args, api, extraOptions);
+
+  if (result.error?.status === 503 || result.error?.originalStatus === 503) {
+    sessionStorage.removeItem("authToken");
+    // window.location.href = "/underMaintenance";
+  }
+
+  if (result.error?.status === 500 || result.error?.originalStatus === 500) {
+    sessionStorage.removeItem("authToken");
+    // window.location.href = "/serverError";
+  }
+  return result;
+};
