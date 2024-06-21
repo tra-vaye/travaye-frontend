15e5254b8e4b77d75ee9baa5d75669d609ffe3f6
Izima01 <kingsleyizima@gmail.com>
2024-06-22 00:39:04 +0100

    Added the progress bar like the design

---
+++ src/components/UI/Progress/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Progress = ({ step }) => {
+  return (
+    <div className="w-[23rem]">
+        <div className="flex justify-between mb-2">
+            <h5 className="text-lg">Progress</h5>
+            <p className="!text-[#9D9D9D] text-lg">{step}/3</p>
+        </div>
+        <div className={`h-2.5 w-full relative bg-[#9D9D9D] rounded-[2.8rem] after:content:[''] after:bg-[#E9A309] ${step === 3 ? 'after:w-full' : `after:w-${step}/3`} after:absolute after:left-0 after:h-full after:rounded-[2.8rem]`} />
+    </div>
+  )
+}
+
+export default Progress
